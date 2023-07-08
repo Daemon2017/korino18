e8943d2889a5d884b12583d2ecb97f6cd2a089f2
Дмитрий Русаков <dmrusakov@samokat.ru>
2023-07-08 21:10:55 +0300

    Исправлена ошибка

--- main.py
+++ main.py
@@ -204,8 +204,9 @@ def get_yearly_population():
     for year in range(start_year, end_year + 1):
         year_data = data[data['Год рождения'] <= year]
         if year_data.size > 0:
-            only_males_data = year_data[~year_data['Женщина']]
-            only_males_data = only_males_data[(only_males_data['Год выбытия'] > year) | (only_males_data['Год выбытия'].isnull())]
+            only_males_data = year_data[year_data['Женщина'] != True]
+            only_males_data = only_males_data[(only_males_data['Год выбытия'] > year) |
+                                              (only_males_data['Год выбытия'].isnull())]
             year_to_population = pd.concat(
                 [year_to_population, pd.DataFrame.from_dict(dict(Год=[year], Население=[len(only_males_data)]))],
                 ignore_index=True)
